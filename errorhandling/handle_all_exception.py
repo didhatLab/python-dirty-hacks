17a917aeda4cb4db1e04e4a91fb207f1cad2f05a
Daniil Solynin <didhat82@gmail.com>
2022-10-23 18:18:13 +0300

    add exception handler hack

---
+++ errorhandling/handle_all_exception.py
@@ -0,0 +1,16 @@
+def func_with_error():
+
+    a = 10 / 0
+    return a
+
+if __name__ == "__main__":
+
+    try:
+        func_with_error()
+    except (Exception,):
+        pass
+
+    print("Handle all exceptions with one except and cheat PEP")
+    print("IDE has not anny complains about this code :)")
+    print("Sometimes it is usefull, when you need to write your code fast")
+    print("However it is really dirty!")
