36b0d966a9151138789aa9a17c8d7bedc283d255
Daniil Solynin <didhat82@gmail.com>
2022-10-30 22:00:06 +0300

    add classes hack

---
+++ interesting/classes_with_global_state.py
@@ -0,0 +1,26 @@
+from typing import List
+
+class DirtyClass:
+
+    def __init__(self, name: str, glob_state: List = []):
+        self.name = name
+        self.glob_state = glob_state
+
+
+if __name__ == "__main__":
+
+    dirty_1 = DirtyClass("Dan")
+    dirty_1.glob_state.append(1)
+    dirty_2 = DirtyClass("Jojo")
+    print("dirty_2 already have state from dirty_1")
+    print(dirty_2.glob_state)
+    dirty_2.glob_state.append(2)
+    print("dirty_1 will take this also")
+    print(dirty_1.glob_state)
+
+    dirty_3 = DirtyClass("kek")
+    print("new dirty_3 also get glob_state :)" )
+
+    dirty_4 = DirtyClass("no_glob", [])
+    print("however dirty_4 has not glob, because we give new list")
+    print(dirty_4.glob_state)
