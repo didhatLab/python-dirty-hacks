fa16196c1e5de861fd8ef8494a3dfae04f59f5e9
Daniil Solynin <didhat82@gmail.com>
2022-11-09 23:23:05 +0300

    add some about concurent work

---
+++ asyncio/threadbasic.py
@@ -0,0 +1,17 @@
+import threading
+
+def hello_from_thread():
+    print(f"Hello from thread {threading.current_thread()}")
+
+
+hello_thread = threading.Thread(target=hello_from_thread)
+hello_thread.start()
+
+total_thread = threading.active_count()
+thread_name = threading.current_thread().name
+
+print(f"Python is currently running {total_thread} thread(s)")
+print(f"The current thread is {thread_name}")
+
+hello_thread.join()
+
